46fb348893f7e8e959c222ea20e426d9df73c929
Paul Mach <paulmach@gmail.com>
2017-05-29 19:06:40 -0700

    osm.Bounds: constructor for map tiles

--- bounds.go
+++ bounds.go
@@ -1,11 +1,54 @@
 package osm
 
+import (
+	"errors"
+	"math"
+)
+
 // Bounds are the bounds of osm data as defined in the xml file.
 type Bounds struct {
 	MinLat float64 `xml:"minlat,attr"`
 	MaxLat float64 `xml:"maxlat,attr"`
 	MinLon float64 `xml:"minlon,attr"`
 	MaxLon float64 `xml:"maxlon,attr"`
+}
+
+// NewBoundFromTile creates a bound given an online map tile index.
+func NewBoundFromTile(x, y, z uint64) (*Bounds, error) {
+	maxIndex := uint64(1) << z
+	if x >= maxIndex {
+		return nil, errors.New("osm: x index out of range for this zoom")
+	}
+	if y >= maxIndex {
+		return nil, errors.New("osm: y index out of range for this zoom")
+	}
+
+	shift := 31 - z
+	if z > 31 {
+		shift = 0
+	}
+
+	lon1, lat1 := scalarInverse(x<<shift, y<<shift, 31)
+	lon2, lat2 := scalarInverse((x+1)<<shift, (y+1)<<shift, 31)
+
+	return &Bounds{
+		MinLat: lat2,
+		MaxLat: lat1,
+		MinLon: lon1,
+		MaxLon: lon2,
+	}, nil
+}
+
+func scalarInverse(x, y, level uint64) (lng, lat float64) {
+	var factor uint64
+
+	factor = 1 << level
+	maxtiles := float64(factor)
+
+	lng = 360.0 * (float64(x)/maxtiles - 0.5)
+	lat = (2.0*math.Atan(math.Exp(math.Pi-(2*math.Pi)*(float64(y))/maxtiles)))*(180.0/math.Pi) - 90.0
+
+	return lng, lat
 }
 
 // ContainsNode returns true if the node is within the bound.
--- bounds_test.go
+++ bounds_test.go
@@ -1,6 +1,31 @@
 package osm
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewBoundFromTile(t *testing.T) {
+	bounds, _ := NewBoundFromTile(7, 8, 9)
+
+	// check 9 tiles around bounds
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			t.Run(fmt.Sprintf("i %d j %d", i, j), func(t *testing.T) {
+				n := centroid(mustBounds(t, uint64(7+i), uint64(8+j), 9))
+				if i == 0 && j == 0 {
+					if !bounds.ContainsNode(n) {
+						t.Errorf("should contain point")
+					}
+				} else {
+					if bounds.ContainsNode(n) {
+						t.Errorf("should not contain point")
+					}
+				}
+			})
+		}
+	}
+}
 
 func TestBoundsContainsNode(t *testing.T) {
 	b := &Bounds{}
@@ -21,5 +46,21 @@ func TestBoundsContainsNode(t *testing.T) {
 
 	if v := b.ContainsNode(&Node{Lon: 1}); v {
 		t.Errorf("should not contain node outside bounds")
+	}
+}
+
+func mustBounds(t *testing.T, x, y, z uint64) *Bounds {
+	bounds, err := NewBoundFromTile(x, y, z)
+	if err != nil {
+		t.Fatalf("invalid bound: %v", err)
+	}
+
+	return bounds
+}
+
+func centroid(b *Bounds) *Node {
+	return &Node{
+		Lon: (b.MinLon + b.MaxLon) / 2,
+		Lat: (b.MinLat + b.MaxLat) / 2,
 	}
 }
