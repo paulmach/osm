e6d92b4892e985df4b97e6e6a83b2ab5c403873d
Paul Mach <paulmach@gmail.com>
2017-07-25 22:41:54 -0700

    osmpbf: prealloc tags during unpacking
    Saves about 3% of allocs and 6-12% performance improvement

--- osmpbf/decode_tag.go
+++ osmpbf/decode_tag.go
@@ -1,6 +1,8 @@
 package osmpbf
 
-import osm "github.com/paulmach/go.osm"
+import (
+	osm "github.com/paulmach/go.osm"
+)
 
 func extractTags(stringTable []string, keyIDs, valueIDs []uint32) osm.Tags {
 	if len(keyIDs) == 0 {
@@ -27,7 +29,22 @@ type tagUnpacker struct {
 // Next creates the tags from the stringtable and array of IDs.
 // Used in DenseNodes encoding.
 func (tu *tagUnpacker) Next() osm.Tags {
-	var tags osm.Tags
+	index := tu.index
+	for index < len(tu.keysVals) {
+		if tu.keysVals[index] == 0 {
+			break
+		}
+
+		index += 2
+	}
+
+	count := index - tu.index
+	if count == 0 {
+		tu.index++
+		return nil
+	}
+
+	tags := make(osm.Tags, 0, count/2)
 	for tu.index < len(tu.keysVals) {
 		keyID := tu.keysVals[tu.index]
 		tu.index++
