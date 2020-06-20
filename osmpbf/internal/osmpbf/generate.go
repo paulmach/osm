9ce269ab9c726f256e730ca823b6d02f25eade3c
Olaf Flebbe <of@oflebbe.de>
2020-06-20 22:28:59 +0200

    update gogo protobuf, generate line

--- osmpbf/internal/osmpbf/generate.go
+++ osmpbf/internal/osmpbf/generate.go
@@ -2,4 +2,4 @@ package osmpbf
 
 // gogo protobuf seems to be about 10% faster
 // https://github.com/gogo/protobuf
-//go:generate protoc --proto_path=../../../../../..:../../../../../gogo/protobuf/protobuf:. --gogofaster_out=. fileformat.proto osmformat.proto
+//go:generate protoc --proto_path=$GOPATH/src/:$GOPATH/src/github.com/gogo/protobuf/protobuf:. --gogofaster_out=.  fileformat.proto osmformat.proto
