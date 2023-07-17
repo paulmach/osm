e879abc73a056105a0375e902658e232f3c44100
Allan Glen <allan@allanglen.com>
2023-07-17 11:52:02 -0600

    Add json struct tags for marshal/unmarshal to json

--- tag.go
+++ tag.go
@@ -23,8 +23,8 @@ var UninterestingTags = map[string]bool{
 
 // Tag is a key+value item attached to osm nodes, ways and relations.
 type Tag struct {
-	Key   string `xml:"k,attr"`
-	Value string `xml:"v,attr"`
+	Key   string `xml:"k,attr" json:"k"`
+	Value string `xml:"v,attr" json:"v,omitempty"`
 }
 
 // Tags is a collection of Tag objects with some helper functions.
