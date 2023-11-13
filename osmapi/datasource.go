73659be68233ded0ca0b7b65735acb45692f3d5b
larsbeck <lars.beckmann@gmail.com>
2023-11-13 10:53:20 +0100

    Remove auto-formatting newline comment

--- osmapi/datasource.go
+++ osmapi/datasource.go
@@ -18,7 +18,6 @@ const BaseURL = "http://api.openstreetmap.org/api/0.6"
 // A RateLimiter is something that can wait until its next allowed request.
 // This interface is met by `golang.org/x/time/rate.Limiter` and is meant
 // to be used with it. For example:
-//
 //		// 10 qps
 //		osmapi.DefaultDatasource.Limiter = rate.NewLimiter(10, 1)
 type RateLimiter interface {
