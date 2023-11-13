48c0be54b812cd4365231b500dab94449ed5fc42
larsbeck <lars.beckmann@gmail.com>
2023-11-13 10:52:23 +0100

    Revert auto-formatting related changes

--- osmapi/datasource.go
+++ osmapi/datasource.go
@@ -19,8 +19,8 @@ const BaseURL = "http://api.openstreetmap.org/api/0.6"
 // This interface is met by `golang.org/x/time/rate.Limiter` and is meant
 // to be used with it. For example:
 //
-//	// 10 qps
-//	osmapi.DefaultDatasource.Limiter = rate.NewLimiter(10, 1)
+//		// 10 qps
+//		osmapi.DefaultDatasource.Limiter = rate.NewLimiter(10, 1)
 type RateLimiter interface {
 	Wait(context.Context) error
 }
